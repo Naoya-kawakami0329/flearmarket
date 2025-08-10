650907749ce0eba88c12e176a9856c9fe51f468a
Kawakaminaoya <kawakaminaoya@example.com>
2025-08-10 23:54:43 +0900

    deleteメソッド

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -25,4 +25,13 @@ export class ItemsService {
     item.status = 'SOLD_OUT';
     return item;
   }
+
+  delete(id: string): Item | undefined {
+    const item = this.findById(id);
+    if (!item) {
+      return undefined;
+    }
+    this.items = this.items.filter((item) => item.id !== id);
+    return item;
+  }
 }
